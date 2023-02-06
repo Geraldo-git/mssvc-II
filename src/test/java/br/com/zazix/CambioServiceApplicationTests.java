2d71d334b9802f556ad142448d424f1ab06cb8d2
Geraldo-dev <laboratorio.inicial@gmail.com>
2023-02-06 12:06:52 -0300

    Commit estrutura inicial

---
+++ src/test/java/br/com/zazix/CambioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zazix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CambioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
