a095bbb24438a22a89f1b226477778f95eb5e267
Geraldo-dev <laboratorio.inicial@gmail.com>
2023-02-12 09:51:01 -0300

    Configuracao OpenAPI

--- book-service/src/main/java/br/com/zazix/controllers/BookController.java
+++ book-service/src/main/java/br/com/zazix/controllers/BookController.java
@@ -10,7 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.zazix.model.Book;
 import br.com.zazix.proxy.CambioProxy;
 import br.com.zazix.repositories.BookRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
+@Tag(name = "Book Endpoint")
 @RestController
 @RequestMapping("book-service")
 public class BookController {
@@ -22,6 +25,7 @@ public class BookController {
 	@Autowired
 	CambioProxy proxy;
 
+	@Operation(summary = "Find a specific book by your id")
 	@GetMapping(value = "/{id}/{currency}")
 	public Book findBook(@PathVariable("id") Long id, @PathVariable("currency") String currency) {
 
